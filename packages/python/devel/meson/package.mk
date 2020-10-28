466c129f5320ddc5acf67fcd761707d722e2a147
SqTER-PL <26832944+SqTER-PL@users.noreply.github.com>
2020-10-28 18:51:14 +0100

    meson: update to meson-0.53.1

--- packages/python/devel/meson/package.mk
+++ packages/python/devel/meson/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="meson"
-PKG_VERSION="0.49.1"
-PKG_SHA256="e90c8ee46109d3b9d9a12c76c65811d4a7f7e18503f780eb301866e43d9052cb"
+PKG_VERSION="0.53.1"
+PKG_SHA256="ec1ba33eea701baca2c1607dac458152dc8323364a51fdef6babda2623413b04"
 PKG_LICENSE="Apache"
 PKG_SITE="http://mesonbuild.com"
 PKG_URL="https://github.com/mesonbuild/meson/releases/download/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
